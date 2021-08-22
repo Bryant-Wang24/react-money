8edb01ee0b2067a7b2fefcbffe4fc722b13b6a1b
Bryant-Wang24 <485969746@qq.com>
2021-08-22 21:39:26 +0800

    消除重复引入，直接require一整个icons目录

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,7 +1,5 @@
-
-require('icons/label.svg')
-require('icons/money.svg')
-require('icons/chart.svg')
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 type Props ={
     name:string
