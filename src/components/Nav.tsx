d7ad9ba5d54b318bb631e1139f932d5d107b3880
Bryant-Wang24 <485969746@qq.com>
2021-08-22 19:11:53 +0800

    整理下代码

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-// import x from 'icons/label.svg'
+// import x from 'icons/label.svg'      因为tree shaking的缘故，改用require来引入svg
 // console.log(x);
 require('icons/label.svg')
 require('icons/money.svg')
@@ -11,20 +11,19 @@ const NavWrapper = styled.nav`
   ul{
     display: flex;
     li{
-      width: 33.3333%;
+      width: 33.3333%;  
       text-align: center;
       a{
         padding: 6px 0;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      .icon{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .icon{
           width: 24px;
           height: 24px;
+        }
       }
-      }
-      
     }
   }
 `
@@ -33,27 +32,30 @@ const Nav = ()=>{
     return(
         <NavWrapper>
             <ul>
-            <li>
-            <Link to="/tags">
-            <svg className="icon">
-                <use xlinkHref="#label"/>
-            </svg>
-              标签页</Link>
-            </li>
-            <li>
-            <Link to="/money">
-            <svg className="icon">
-                <use xlinkHref="#money"/>
-            </svg>
-              记账页</Link>
-            </li>
-            <li>
-            <Link to="/statistics">
-            <svg className="icon">
-                <use xlinkHref="#chart"/>
-            </svg>
-              统计页</Link>
-            </li>
+                <li>
+                    <Link to="/tags">
+                        <svg className="icon">
+                            <use xlinkHref="#label"/>
+                        </svg>
+                        标签页
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/money">
+                        <svg className="icon">
+                            <use xlinkHref="#money"/>
+                        </svg>
+                        记账页  
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/statistics">
+                        <svg className="icon">
+                            <use xlinkHref="#chart"/>
+                        </svg>
+                        统计页
+                    </Link>
+                </li>
           </ul>
         </NavWrapper>
     )
