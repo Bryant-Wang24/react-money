06910adb1dc90432c167bae09a0951f975623c65
Bryant-Wang24 <485969746@qq.com>
2021-08-22 14:28:33 +0800

    删掉没用的文件，保持整洁

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import Box from 'components/Box';
-import Button from 'components/Button';
+
 import React from 'react';
-import './App.css';
 
 
 function App() {
   return (
-    <div>
-      <Box>
-        <Button>风萧萧兮易水寒</Button>
-      </Box>
+    <div>hello
     </div>
   );
 }
--- src/components/Box.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Box = styled.div`
-border: 1px solid black;
-margin: 50px;
-box-shadow: 0 0 5px rgba(black);
-`
-export default Box
