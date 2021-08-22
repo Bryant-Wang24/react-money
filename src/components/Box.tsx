83e1a4b0d6bf487d86c160202d75aafb39d95d96
Bryant-Wang24 <485969746@qq.com>
2021-08-22 14:23:17 +0800

    使用style-components实现css in js

---
+++ src/components/Box.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+border: 1px solid black;
+margin: 50px;
+box-shadow: 0 0 5px rgba(black);
+`
+export default Box
---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+color:red;
+font-size: large;
+background: pink;
+&:hover{
+  background: black;
+}
+`
+export default Button
