932d611743ce5f44b9ae742e61e4c1ac06d5fff0
Bryant-Wang24 <485969746@qq.com>
2021-08-23 20:51:03 +0800

    记账页面html

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -18,25 +18,47 @@ const Money = () =>{
     return(
       <Layout>
           <TagsSection>
-            <ol>
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-            </ol> 
-            <button>新增标签</button>
-          </TagsSection>
-          <NotesSection>
-              <label htmlFor="">备注</label>
-              <input type="text" />
-          </NotesSection>
-          <CategorySection>
-            <div>支出</div>
-            <div>收入</div>
-          </CategorySection>
-          <NumberPadSection>
-              数字面板
-          </NumberPadSection>
+        <ol>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ol>
+        <button>新增标签</button>
+      </TagsSection>
+      <NotesSection>
+        <label>
+          <span>备注</span>
+          <input type="text"/>
+        </label>
+      </NotesSection>
+      <CategorySection>
+        <ul>
+          <li>支出</li>
+          <li>收入</li>
+        </ul>
+      </CategorySection>
+      <NumberPadSection>
+        <div>
+          100
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </NumberPadSection>
       </Layout>
     ) 
 }
