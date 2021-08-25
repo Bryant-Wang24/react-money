88742be5694b27d9975cd5f4099479cd018c50ce
Bryant-Wang24 <485969746@qq.com>
2021-08-25 22:39:24 +0800

    完成标签的选择与新增功能

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useState } from "react";
 
 const Wrapper = styled.section`
   background: #FFFFFF; padding: 12px 16px;
@@ -10,6 +10,9 @@ const Wrapper = styled.section`
        background: #D9D9D9; border-radius: 18px;
        display:inline-block; padding: 3px 18px; 
        font-size: 14px; margin: 8px 12px;
+       &.selected{
+         background: pink;
+       }
     }
   }
   > button{
@@ -19,15 +22,33 @@ const Wrapper = styled.section`
   }
 `
 const TagsSection: React.FC= () => {
+  // 此处的”<string[]>“表示“['衣','食','住','行']“是一个字符串数组
+  const [tags,setTags] = useState<string[]>(['衣','食','住','行'])
+  const [selectedTags,setSelectedTags] = useState<string[]>([])
+  const onToggleTag = (tag:string)=>{
+    const index = selectedTags.indexOf(tag)
+    if(index>=0){
+      // 如果 tag 已被选中，就复制所有没有被选中的 tag，作为新的 selectedTag
+      setSelectedTags(selectedTags.filter(t=>t!==tag))
+    }else{
+      setSelectedTags([...selectedTags,tag])
+    }
+  }
+  const addTag = ()=>{
+    const tagName = window.prompt('请添加一个新的标签')
+    if(tagName){
+      setTags([...tags,tagName])
+    }
+  }
+  const getClass = (tag:string)=>selectedTags.indexOf(tag)>=0 ? 'selected':''
     return(
         <Wrapper>
             <ol>
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
+                {tags.map(tag=>
+                <li key={tag} onClick={()=>{onToggleTag(tag)}} 
+                className={getClass(tag)}>{tag}</li>)}
             </ol>
-            <button>新增标签</button>
+            <button onClick={addTag}>新增标签</button>
         </Wrapper>
     )
 }
