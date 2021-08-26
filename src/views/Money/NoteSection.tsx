c5d24e9b7da2cfd2f1a53352afcbfc526a8f3697
Bryant-Wang24 <485969746@qq.com>
2021-08-26 22:03:04 +0800

    受控组件

---
+++ src/views/Money/NoteSection.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+    background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display:flex;
+    align-items: center;
+    > span { margin-right: 16px; white-space: nowrap; }
+    > input {
+      display:block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`
+
+const NoteSection:React.FC = ()=>{
+  const [note,setNote] = useState('')
+  console.log(note);
+  return(
+    <Wrapper>
+      <label>
+          <span>备注</span>
+          <input type="text" value={note} onChange={(e)=>setNote(e.target.value)} placeholder="在这里添加备注"/>
+        </label>
+    </Wrapper>
+  )
+}
+
+export default NoteSection
