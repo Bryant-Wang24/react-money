33d036d0964a4ade63cd6b370faedd72bcb0559d
Bryant-Wang24 <485969746@qq.com>
2021-08-28 22:58:18 +0800

    数字面板的功能逻辑基本完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,44 +1,23 @@
-import Layout from "components/Layout"
-import styled from "styled-components"
-import CategorySection from "./Money/CategorySection"
-import NoteSection from "./Money/NoteSection"
-import NumberPadSection from "./Money/NumberPadSection"
-import TagsSection from "./Money/TagsSection"
-
+import Layout from "components/Layout";
+import styled from "styled-components";
+import CategorySection from "./Money/CategorySection";
+import NoteSection from "./Money/NoteSection";
+import NumberPadSection from "./Money/NumberPadSection";
+import TagsSection from "./Money/TagsSection";
 
 const MyLayout = styled(Layout)`
-  display:flex;
+  display: flex;
   flex-direction: column;
-`
+`;
 
-const Money = () =>{
-    return(
-      <MyLayout>
-          <TagsSection/>
-      <NoteSection/>
-      <CategorySection/>  
-      <NumberPadSection>
-        <div className="output">
-          100
-        </div>
-        <div className="pad clearfix">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className="ok">OK</button>
-          <button className="zero">0</button>
-          <button className="dot">.</button>
-        </div>
-      </NumberPadSection>
-      </MyLayout>
-    ) 
-}
-export default Money
+const Money = () => {
+  return (
+    <MyLayout>
+      <TagsSection />
+      <NoteSection />
+      <CategorySection />
+      <NumberPadSection />
+    </MyLayout>
+  );
+};
+export default Money;
