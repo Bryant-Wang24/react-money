f6f638058910c414de106a7b8c44ab46b52a8f63
Bryant-Wang24 <485969746@qq.com>
2021-08-30 21:22:01 +0800

    创建tag页面，react路由，exact精准匹配

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,22 @@ import Money from 'views/Money';
 import NoMatch from 'views/NoMatch';
 import Statistics from 'views/Statistics';
 import Tags from 'views/Tags';
+import {Tag} from './views/tag';
 
 export default function App() {
   return (
     <Router>
         <Switch>
-          <Route path="/tags">
+          <Route exact path="/tags">
             <Tags />
           </Route>
-          <Route path="/money">
+          <Route exact path="/tags/:tag">
+            <Tag />
+          </Route>
+          <Route exact path="/money">
             <Money />
           </Route>
-          <Route path="/statistics">
+          <Route exact path="/statistics">
             <Statistics />
           </Route>
           <Redirect exact from="/" to="/money" />
---
+++ src/views/tag.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border: 1px solid red;
+`
+
+const Tag = ()=>{
+  return(
+    <Wrapper>hi</Wrapper>
+  )
+}
+export {Tag}
