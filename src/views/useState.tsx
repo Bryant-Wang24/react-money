b35a18c0ab622f5a4e63b2404dcf521fb718ed8b
Bryant-Wang24 <485969746@qq.com>
2021-08-31 18:42:01 +0800

    创建一个id自增的函数

--- src/views/useState.tsx
+++ src/views/useState.tsx
@@ -1,12 +1,17 @@
 import {useState} from 'react';
-
+let id = 0
+const createId = ()=>{
+  id+=1
+  return id
+}
+const data = [
+  {id:createId(),name:'衣'},
+  {id:createId(),name:'食'},
+  {id:createId(),name:'住'},
+  {id:createId(),name:'行'}
+]
 const useTags = ()=>{
-  const [tags,setTags] = useState<{id:number,name:string}[]>([
-    {id:1,name:'衣'},
-    {id:2,name:'食'},
-    {id:3,name:'住'},
-    {id:4,name:'行'}
-      ]
+  const [tags,setTags] = useState<{id:number,name:string}[]>(data
   )
   return {tags,setTags}
 }
