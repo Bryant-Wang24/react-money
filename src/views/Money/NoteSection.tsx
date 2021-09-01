e36866aba4abeee6ee7143ccccfa676d5c4221ba
Bryant-Wang24 <485969746@qq.com>
2021-09-01 22:44:39 +0800

    创建单独的 Input

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,22 +1,11 @@
-import React, {ChangeEventHandler, useRef} from 'react';
+import React, {ChangeEventHandler} from 'react';
 import styled from "styled-components";
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
     background: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  > label {
-    display:flex;
-    align-items: center;
-    > span { margin-right: 16px; white-space: nowrap; }
-    > input {
-      display:block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `
 type Props = {
   value:string,
@@ -31,10 +20,7 @@ const NoteSection:React.FC<Props> = (props)=>{
   // console.log(note);
   return(
     <Wrapper>
-      <label>
-          <span>备注</span>
-          <input  type="text" value={note} onChange={onChange} />
-        </label>
+          <Input label='备注' type="text" value={note} onChange={onChange} />
     </Wrapper>
   )
 }
